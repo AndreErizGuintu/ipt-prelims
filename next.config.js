4f7f4afd1ad8acb1ac28565162ad00ce6aad5057
Guintu <andreguintu26@gmail.com>
2025-08-06 08:47:20 +0800

    first com

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+
+    typescript: {
+        // Set to true to skip type checking during the build
+        ignoreBuildErrors:  true
+    },
+
+    eslint: {
+        // Set to true to skip linting during the build
+        ignoreDuringBuilds: true
+    },
+};
+
+export default config;
---
+++ src/server/db/schema.ts
@@ -0,0 +1,31 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import { index, pgTableCreator } from "drizzle-orm/pg-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = pgTableCreator(
+  (name) => `ipt-3aguintu-gallery_${name}`,
+);
+
+export const images = createTable(
+  "image",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    filename: d.varchar({ length: 256 }),
+    imageUrl: d.varchar({ length: 1200 }).notNull(),
+    userId: d.varchar({ length: 64 }).notNull(),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  // (t) => [index("name_idx").on(t.name)],
+);
